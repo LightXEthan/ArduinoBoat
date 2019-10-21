bb8d6947e505544dc00aca0edb1e418f391aa3f7
LightXEthan <lightxethan@gmail.com>
2019-10-21 15:44:15 +0800

    Add final working code

--- project/project.ino
+++ project/project.ino
@@ -2,10 +2,6 @@
 #include <stdlib.h>
 #include <SparkFun_MAG3110.h>
 
-// These may need to be different depending on the motors
-// left/right adjusting, turning
-
-
 int trigPin = 6;
 int echoPin = 7;
 long duration, cm;
@@ -53,12 +49,13 @@ void sonar() {
   return;
 }
 
+// Calibrates the magnometer
 void calibration() {
   while (!mag.isCalibrated()) 
   {
     if(!mag.isCalibrating()) //And we're not currently calibrating
     {
-      Serial.println("Entering calibration mode");
+      //Serial.println("Entering calibration mode");
       mag.enterCalMode(); //This sets the output data rate to the highest possible and puts the mag sensor in active mode
     }
     else
@@ -67,19 +64,19 @@ void calibration() {
       //This will automatically exit calibration
       //You can terminate calibration early by calling mag.exitCalMode();
       mag.calibrate(); 
-      Serial.println("Calibrating!");
+      //Serial.println("Calibrating!");
     }
     delay(100);
   }
-  bearing = mag.readHeading();
-  Serial.println("Calibrated!!");
-  Serial.print(mag.readOffset(mag.x_scale));
-  Serial.print(mag.readOffset(mag.y_scale));
+  //Serial.println("Calibrated!!");
+  //Serial.print(mag.readOffset(mag.x_scale));
+  //Serial.print(mag.readOffset(mag.y_scale));
   // set calibration data using mag.setOffset(axis, offset);
   digitalWrite(13, HIGH); // led turns on
-  delay(5000); // Wait five seconds
+  delay(3000); // Wait five seconds
   
   // gets the forward and backwards directions
+  bearing = mag.readHeading();
   forward = bearing;
   if (forward > 0) {
     // forward is positive
@@ -91,7 +88,7 @@ void calibration() {
 
 void setup() {
   //Serial Port begin
-  Serial.begin (9600);
+  //Serial.begin (9600);
 
   // Setup I2C bus for magnometer
   Wire.begin();             //setup I2C bus
@@ -108,6 +105,10 @@ void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 
+  analogWrite(E1, 0);
+  analogWrite(E2, 0);
+  delay(100);
+
   // Motor
   pinMode(E1, OUTPUT);
   pinMode(E2, OUTPUT);
@@ -119,7 +120,7 @@ void setup() {
 
   // magnometer 
   mag.initialize(); //Initialize the MAG3110
-  thresh = 20;
+  thresh = 10;
   isForward = 1;
   
   // calibrate mag
@@ -127,87 +128,124 @@ void setup() {
 }
 
 // Move one motor backwards and one forward, led blinks while turning
-void turning() {
+void turningLeft() {
   // Run the motors, given the motor speed
-  left_motor = 220;
-  right_motor = 200;
-  int turning_time = 3; // seconds
-  
-  analogWrite(E1, left_motor); // Run in half speed
-  analogWrite(E2, right_motor); // Run in full speed
+  int turning_time = 15; // 200ms * turning_time
 
   for (int i = 0; i < turning_time; i++) {
+    bearing = mag.readHeading();
+    if (thresh > abs(backward - bearing) || (360 - thresh) < abs(backward - bearing)) {
+      //Serial.println("Turned");
+      break;
+    }
+    
+    digitalWrite(E1, HIGH);
+    digitalWrite(E2, HIGH);
+
     digitalWrite(13, HIGH); // led turns on
     
     digitalWrite(I1, HIGH);
     digitalWrite(I2, LOW);
     digitalWrite(I3, LOW);
     digitalWrite(I4, HIGH);
-    delay(1000);
+    delay(200);
     
     digitalWrite(13, LOW); // led turns off
-
-    analogWrite(E1, 0);
-    analogWrite(E2, 0);
-    delay(500);
+    
   }
-  
-
+  //analogWrite(E1, right_motor); // Run in half speed 130 stal
+  digitalWrite(E1, LOW);
+  //analogWrite(E2, left_motor); // Run in full speed
+  digitalWrite(E2, LOW);
+  delay(1000);
 }
 
-void driveMotor(int left_motor, int right_motor) {
-  
+// Move one motor backwards and one forward, led blinks while turning
+void turningRight() {
   // Run the motors, given the motor speed
-  analogWrite(E1, left_motor); // Run in half speed
-  analogWrite(E2, right_motor); // Run in full speed
+  int turning_time = 15; // 200ms * turning_time
+
+  for (int i = 0; i < turning_time; i++) {
+    bearing = mag.readHeading();
+    if (thresh > abs(backward - bearing) || (360 - thresh) < abs(backward - bearing)) {
+      Serial.println("Turned");
+      break;
+    }
+    
+    digitalWrite(E1, HIGH);
+    digitalWrite(E2, HIGH);
+
+    digitalWrite(13, HIGH); // led turns on
+    
+    digitalWrite(I1, LOW);
+    digitalWrite(I2, HIGH);
+    digitalWrite(I3, HIGH);
+    digitalWrite(I4, LOW);
+    delay(200);
+    
+    digitalWrite(13, LOW); // led turns off
+    
+  }
+
+  // Stops the motors
+  digitalWrite(E1, LOW);
+  digitalWrite(E2, LOW);
+  delay(1000);
+}
+
+void driveMotor() {
+  //Serial.println(" Forward");
+
+  // Run the motors, given the motor speed
+  digitalWrite(E1, HIGH);
+  digitalWrite(E2, HIGH);
   
   digitalWrite(I1, HIGH);
   digitalWrite(I2, LOW);
   digitalWrite(I3, HIGH);
   digitalWrite(I4, LOW);
+  delay(200);
+
+  // Stops the motors
+  digitalWrite(E1, LOW);
+  digitalWrite(E2, LOW);
   delay(1000);
-  
-  analogWrite(E1, 0);
-  analogWrite(E2, 0);
-  delay(500);
 
 }
 
 void driveLeft() {
-  left_motor = 180;
-  right_motor = 220;
   // Run the motors, given the motor speed
-  analogWrite(E1, left_motor); // Run in half speed
-  analogWrite(E2, right_motor); // Run in full speed
+  digitalWrite(E1, HIGH);
+  digitalWrite(E2, HIGH);
   
   digitalWrite(I1, HIGH);
   digitalWrite(I2, LOW);
-  digitalWrite(I3, HIGH);
-  digitalWrite(I4, LOW);
+  digitalWrite(I3, LOW);
+  digitalWrite(I4, HIGH);
+  delay(200);
+
+  // Stops the motors
+  digitalWrite(E1, LOW);
+  digitalWrite(E2, LOW);
   delay(1000);
-  
-  analogWrite(E1, 0);
-  analogWrite(E2, 0);
-  delay(500);
 
 }
 
 void driveRight() {
-  left_motor = 220;
-  right_motor = 180;
   // Run the motors, given the motor speed
-  analogWrite(E1, left_motor); // Run in half speed
-  analogWrite(E2, right_motor); // Run in full speed
+  digitalWrite(E1, HIGH);
+  digitalWrite(E2, HIGH);
   
-  digitalWrite(I1, HIGH);
-  digitalWrite(I2, LOW);
+  digitalWrite(I1, LOW);
+  digitalWrite(I2, HIGH);
   digitalWrite(I3, HIGH);
   digitalWrite(I4, LOW);
+  delay(200);
+
+  // Stops the motors
+  digitalWrite(E1, LOW);
+  digitalWrite(E2, LOW);
   delay(1000);
-  
-  analogWrite(E1, 0);
-  analogWrite(E2, 0);
-  delay(500);
 
 }
 
@@ -216,32 +254,48 @@ void loop() {
   sonar();
 
   // print ultra sonic sensor
+  /*
   Serial.print("duration: ");
   Serial.print(duration);
   Serial.print("\t");
   Serial.print(cm);
   Serial.print("cm");
-
+  */
   // Detects wall, in cm
-  if (cm < 40) {
+  if (cm < 115) {
     if (!isForward) {
-      // Stops when going back and finding a wall
-      Serial.println("STOP!");
-      driveMotor(0,0); // stop the motors
-      digitalWrite(13, LOW); // led turns off
-      while (1);
+      if (cm < 70) {
+        // Stops when going back and finding a wall
+        digitalWrite(13, LOW); // led turns off
+        delay(1000);
+        digitalWrite(13, HIGH); // led turns off
+        delay(1000);
+        digitalWrite(13, LOW); // led turns off
+        while (1);
+      }
+    } else {
+      //Serial.print("\tWall detected!");
+      isForward = 0;
+      bearing = mag.readHeading();
+      // Perform turning
+    
+      if ( 0 < (forward - bearing) && (forward - bearing) < 180 || (forward - bearing) < -180) {
+        //Turn right
+        turningRight();
+      } else {
+        //Turn left
+        turningLeft();
+      }
+      
     }
-    Serial.print("\tWall detected!");
-    isForward = 0;
-    // Perform turning
-    turning();
+    
   }
-  Serial.println();
 
   // print magnometer
   int x, y, z;
   
   mag.readMag(&x, &y, &z);
+  /*
   Serial.print("X: ");
   Serial.print(x);
   Serial.print(", Y: ");
@@ -250,56 +304,46 @@ void loop() {
   Serial.print(z);
 
   Serial.print(" Heading: ");
+  */
+  
   bearing = mag.readHeading();
-  Serial.println(bearing);
-
-  // default motor value
-  left_motor = 220;
-  right_motor = 190;
+  //Serial.println(bearing);
 
   // Detects if the boat is not straight
   if (isForward && thresh <= abs(forward - bearing) && (360 - thresh) >= abs(forward - bearing)) {
     // Run PID to adjust
-    Serial.print(forward);
-    Serial.println(" Forward");
-    speed_amount = abs(forward) - abs(bearing);
+    //Serial.print(forward);
+    
     // set the motor speed
     if ( 0 < (forward - bearing) && (forward - bearing) < 180 || (forward - bearing) < -180) {
       // Assume drift to left
-      //left_motor = 0;
+      //right_motor = 0;
+      //Serial.println(" Left");
       driveLeft();
     } else {
-      //right_motor = 0;
+      //left_motor = 0;
+      //Serial.println(" Right");
       driveRight();
     }
-  }
-  else if (isForward) {
-    driveMotor(left_motor, right_motor);
   }
 
   if (!isForward && thresh <= abs(backward - bearing) && (360 - thresh) >= abs(backward - bearing)) {
     // Run PID to adjust
-    Serial.print(backward);
-    Serial.println(" Backward");
-    speed_amount = abs(forward) - abs(bearing);
+    //Serial.print(backward);
+    //Serial.println(" Backward");
+
     if ( 0 < (backward - bearing) && (backward - bearing) < 180 || (backward - bearing) < -180) {
       // Assume drift to left
-      //right_motor = 0;
-      driveRight();
-    } else {
       //left_motor = 0;
       driveLeft();
+    } else {
+      //right_motor = 0;
+      driveRight();
     }
   }
-  else if (!isForward) {
-    driveMotor(left_motor, right_motor);
-  }
+  driveMotor();
+  
 }
 
-
-
-
-
-
-// Reference: https://randomnerdtutorials.com/complete-guide-for-ultrasonic-sensor-hc-sr04/
+// References: https://randomnerdtutorials.com/complete-guide-for-ultrasonic-sensor-hc-sr04/
 // https://learn.sparkfun.com/tutorials/mag3110-magnetometer-hookup-guide
